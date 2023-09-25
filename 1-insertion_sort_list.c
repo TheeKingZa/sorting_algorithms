e33fe36d5ae452432709650dafa3e13b48fbb00c
Pule <proceedingmc@gmail.com>
2023-09-25 10:28:52 +0200

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,43 @@
 #include "sort.h"
-#include <stdio.h>
+
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascending
  * order using the Insertion sort algorithm.
- * @list: A pointer to a pointer to the head of the list
+ * @list: A pointer to a pointer to the head of the linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev, *next;
-	if (list == NULL || *list == NULL)
-		return;
+listint_t *current, *prev, *temp;
 
-/* Initialize 'current, prev and next' to the hnode */
+if (!list || !*list || !(*list)->next)
+return;
 
-	current = (*list)->next;
+current = (*list)->next;
 
-	while (current != NULL)
-	{
-		prev = current->prev;
-		next = current->next;
+while (current)
+{
+temp = current;
+prev = current->prev;
 
-	while (prev != NULL && prev->n > current->n)
-	{
-/* Perform the swap of 'current' and 'prev' */
-		prev->next = next;
-		if (next != NULL)
-			next->prev = prev;
-		current->prev = prev->prev;
-		current->next = prev;
-		if (prev->prev != NULL)
-			prev->prev->next = current;
-		prev->prev = current;
+while (prev && prev->n > temp->n)
+{
+/* Swap prev and temp nodes */
+if (prev->prev)
+prev->prev->next = temp;
+if (temp->next)
+temp->next->prev = prev;
 
-		if (prev == *list)
-/* If 'prev' was the head of the list, update the head */
-		*list = current;
+prev->next = temp->next;
+temp->prev = prev->prev;
+prev->prev = temp;
+temp->next = prev;
 
-		print_list(*list);
-/* Print the list after each swap */
-		prev = current->prev;
-/* Move 'prev' backwards to check previous elements */
-	}
-	current = next;
-/* Move 'current' forward to check the next element*/
-	}
+if (!temp->prev)
+*list = temp;
+
+print_list(*list);
+prev = temp->prev;
+}
+current = current->next;
+}
 }
