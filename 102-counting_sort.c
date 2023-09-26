9cfbb1d61c8c05ee864685a65a22b9c2b76225d1
Pule <proceedingmc@gmail.com>
2023-09-26 11:45:56 +0200

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,39 +8,39 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    int *output = NULL;
-    int max = 0;
-    size_t i;
+int *output = NULL;
+int max = 0;
+size_t i;
 
-    if (!array || size < 2)
-        return;
+if (!array || size < 2)
+return;
 
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
+for (i = 0; i < size; i++)
+{
+if (array[i] > max)
+max = array[i];
+}
 
-    output = malloc(sizeof(int) * (max + 1));
-    if (!output)
-        return;
+output = malloc(sizeof(int) * (max + 1));
+if (!output)
+return;
 
-    for (i = 0; i <= (size_t)max; i++)
-        output[i] = 0;
+for (i = 0; i <= (size_t)max; i++)
+output[i] = 0;
 
-    for (i = 0; i < size; i++)
-        output[array[i]]++;
+for (i = 0; i < size; i++)
+output[array[i]]++;
 
-    for (i = 1; i <= (size_t)max; i++)
-        output[i] += output[i - 1];
+for (i = 1; i <= (size_t)max; i++)
+output[i] += output[i - 1];
 
-    print_array(output, max + 1);
+print_array(output, max + 1);
 
-    for (i = 0; i < size; i++)
-    {
-        array[output[array[i]] - 1] = array[i];
-        output[array[i]]--;
-    }
+for (i = 0; i < size; i++)
+{
+array[output[array[i]] - 1] = array[i];
+output[array[i]]--;
+}
 
-    free(output);
+free(output);
 }
